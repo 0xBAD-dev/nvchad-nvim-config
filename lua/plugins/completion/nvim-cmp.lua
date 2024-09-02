9acb681348fbf2e89aada34356f991956f5ff330
Serhii Prodan <22973227+serpro69@users.noreply.github.com>
2024-09-02 07:45:35 +0200

    Use BufReadPost event in codecompanion plugin
    Follow the white rabbit, i.e. do the same as was done in e8593d2

--- lua/plugins/completion/nvim-cmp.lua
+++ lua/plugins/completion/nvim-cmp.lua
@@ -145,7 +145,7 @@ return {
     {
       "olimorris/codecompanion.nvim",
       enabled = true,
-      event = "User FilePost",
+      event = "BufReadPost",
       dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-treesitter/nvim-treesitter",
