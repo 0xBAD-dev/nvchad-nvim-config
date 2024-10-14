fadca035dd10569319ff35505b3b0906359981f7
serpro69 <22973227+serpro69@users.noreply.github.com>
2024-10-14 10:52:17 +0200

    Enable nvim-lint plugin and add linters

--- lua/plugins/lsp/configs/nvim-lint.lua
+++ lua/plugins/lsp/configs/nvim-lint.lua
@@ -1,15 +1,24 @@
+local linters_by_ft = {
+  javascriptreact = { "eslint" },
+  javascript = { "eslint" },
+  markdown = { "markdownlint" },
+  sh = { "shellcheck" },
+  -- tf = { "tflint", "trivy" },
+  -- terraform = { "tflint", "trivy" },
+  typescript = { "eslint" },
+  typescriptreact = { "eslint" },
+  yaml = { "yamllint" },
+  -- github = { "actionlint" },
+}
+
 ---@type NvPluginSpec
 -- NOTE: Linting
 return {
   "mfussenegger/nvim-lint",
   event = "VeryLazy",
-  cond = false,
+  cond = true,
   config = function()
-    require("lint").linters_by_ft = {
-      -- python = { "flake8" },
-      -- github = { "actionlint" },
-    }
-
+    require("lint").linters_by_ft = linters_by_ft
     vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
       callback = function()
         require("lint").try_lint()
