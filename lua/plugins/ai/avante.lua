f11512dc527c67d6a18eac53be2159b6399da2d1
serpro69 <22973227+serpro69@users.noreply.github.com>
2025-05-12 10:12:35 +0200

    Configure avante models

--- lua/plugins/ai/avante.lua
+++ lua/plugins/ai/avante.lua
@@ -16,6 +16,12 @@ return {
     -- add any opts here
     -- for example
     provider = "gemini",
+    copilot = {
+      model = require("serpro69.plugins_aux").ai_models.copilot.claude_sonnet,
+    },
+    gemini = {
+      model = require("serpro69.plugins_aux").ai_models.gemini.pro,
+    },
     windows = {
       width = 0.2 * vim.o.columns,
     },
