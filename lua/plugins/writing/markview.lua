bf630241b16298c8616d4a3d3d64a9dda4243612
serpro69 <22973227+serpro69@users.noreply.github.com>
2025-09-16 15:32:28 +0200

    Don't load nvim-treesitter before markview
    Fix error reported in :checkhealth markview

--- lua/plugins/writing/markview.lua
+++ lua/plugins/writing/markview.lua
@@ -27,7 +27,7 @@ return {
     -- You will not need this if you installed the
     -- parsers manually
     -- Or if the parsers are in your $RUNTIMEPATH
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-tree/nvim-web-devicons",
+    -- "nvim-treesitter/nvim-treesitter",
+    -- "nvim-tree/nvim-web-devicons",
   },
 }
