e8ddfaaa6a9231036fb85ef1950eba0b5eec28b3
serpro69 <22973227+serpro69@users.noreply.github.com>
2025-10-10 17:41:03 +0200

    Disable auto-session
    It's a bit annoying for my taste,
    and also produces too many errors and conflicts with
    some other stuff, like NvimTree

--- lua/plugins/sessions/auto-session.lua
+++ lua/plugins/sessions/auto-session.lua
@@ -2,6 +2,7 @@
 -- NOTE: Sessions
 return {
   "rmagatti/auto-session",
+  enabled = false,
   lazy = false,
   dependencies = {
     "nvim-telescope/telescope.nvim", -- Only needed if you want to use sesssion lens
